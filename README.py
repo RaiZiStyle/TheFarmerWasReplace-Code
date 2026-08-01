18b4a10363ca45e04f330532dfe0a4bb55c0c1ce
Arthur G.P <arthur.guyotpremel@gmail.com>
2026-08-02 01:20:20 +0200

    Ouspy

--- README.py
+++ README.py
@@ -1,10 +1,8 @@
 # How the fuck does my linter in VSCode work direclty with the "__buildins__.py" ?, i don't get it
-MAIN = "main.py         : Entry point — orchestrates and runs all processes."
-PLANTS = "plants.py       : Per-plant definitions and harvest logic."
-HARVESTER = (
-    "harvester.py    : Priority-based harvesting with MAX value cap and idle fallback."
-)
-UTILS = "utils.py        : Fun functions"
+MAIN        = "main.py         : Entry point — orchestrates and runs all processes."
+PLANTS      = "plants.py       : Per-plant definitions and harvest logic."
+HARVESTER   = "harvester.py    : Priority-based harvesting with MAX value cap and idle fallback."
+UTILS       = "utils.py        : Fun functions"
 
 # TODO : "Add Sunflower (power) and Maze plant types."
 # TODO : "Introduce variable plants with random generation."
