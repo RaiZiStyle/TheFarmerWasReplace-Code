e300b2eb411f7c030477fc4a06d8351998f2ba53
Arthur G.P <arthur.guyotpremel@gmail.com>
2026-08-02 01:21:02 +0200

    Typo

--- README.py
+++ README.py
@@ -1,8 +1,8 @@
 # How the fuck does my linter in VSCode work direclty with the "__buildins__.py" ?, i don't get it
 MAIN        = "main.py         : Entry point — orchestrates and runs all processes."
-PLANTS      = "plants.py       : Per-plant definitions and harvest logic."
-HARVESTER   = "harvester.py    : Priority-based harvesting with MAX value cap and idle fallback."
-UTILS       = "utils.py        : Fun functions"
+PLANTS      = "Plants.py       : Per-plant definitions and harvest logic."
+HARVESTER   = "Harvester.py    : Priority-based harvesting with MAX value cap and idle fallback."
+UTILS       = "Utils.py        : Fun functions"
 
 # TODO : "Add Sunflower (power) and Maze plant types."
 # TODO : "Introduce variable plants with random generation."
