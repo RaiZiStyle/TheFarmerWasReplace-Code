3d754333b59f2f8c722a508ce5b50d6a5a555fd5
Arthur G.P <arthur.guyotpremel@gmail.com>
2026-08-01 23:10:13 +0200

    Revert "Test"
    This reverts commit 6865f02e06c1c214af9c4cd70e5c45045b4c7534.

--- README.py
+++ README.py
@@ -1,7 +1,5 @@
 # How the fuck does my linter in VSCode work direclty with the "__buildins__.py" ?, i don't get it
-
-
-MAIN        = {"FILE" : "main.py",         "DESCRIPTION" : "Entry point — orchestrates and runs all processes."}
+MAIN        = "main.py         : Entry point — orchestrates and runs all processes."
 PLANTS      = "plants.py       : Per-plant definitions and harvest logic."
 HARVESTER   = "harvester.py    : Priority-based harvesting with MAX value cap and idle fallback."
 UTILS       = "utils.py        : Fun functions"
